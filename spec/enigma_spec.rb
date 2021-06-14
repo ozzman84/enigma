a2fe87359670cebc262c3aa5adce2706badd2c1e
Ozzie <mikeosmonson@gmail.com>
2021-06-14 17:20:08 -0600

    Add #encrypt test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,9 +5,20 @@ require 'date'
 SimpleCov.start
 
 RSpec.describe Enigma do
-  it "exists" do
+  it 'exists' do
     enigma = Enigma.new
 
-    expect(enigma).to eq(Enigma)
+    expect(enigma).to be_a(Enigma)
+  end
+
+  it 'can encrypt' do
+    enigma = Enigma.new
+    enigma.encrypt("hello world", "02715", "040895")
+
+    expect(enigma.encrypt).to eq({
+      encryption: "keder ohulw",
+      key:        "02715",
+      date:       "040895"
+    })
   end
 end
