79b2d928233573aeca30122add6f4017f10dacc8
Ozzie <mikeosmonson@gmail.com>
2021-06-14 15:23:23 -0600

    Add #Create_offsets_hash

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -3,9 +3,9 @@ class Shift
               :date
 
   def initalize
-    @key = key
-    @date = date
-    @key_hash = Hash.new(0)
+    @key      = key
+    @date     = date
+    @key_hash = key_hash
   end
 
   def create_key
@@ -17,12 +17,23 @@ class Shift
   end
 
   def create_key_hash(key)
-    key = key.split('').map(&:to_i)
+    key_arr   = key.split('').map(&:to_i)
     @key_hash = {
-      A: key.values_at(0, 1).join.to_i,
-      B: key.values_at(1, 2).join.to_i,
-      C: key.values_at(2, 3).join.to_i,
-      D: key.values_at(3, 4).join.to_i
+      A: key_arr.values_at(0, 1).join.to_i,
+      B: key_arr.values_at(1, 2).join.to_i,
+      C: key_arr.values_at(2, 3).join.to_i,
+      D: key_arr.values_at(3, 4).join.to_i
+    }
+  end
+
+  def create_offsets_hash(date)
+    date_arr  = date.to_i * date.to_i
+    date_arr  = date_arr.to_s.split('')
+    @offsets_hash = {
+      D: date_arr.pop.to_i,
+      C: date_arr.pop.to_i,
+      B: date_arr.pop.to_i,
+      A: date_arr.pop.to_i
     }
   end
 end
