6687c02770f1401637b0fda3b67b35b0109ba480
Ozzie <mikeosmonson@gmail.com>
2021-06-14 12:59:40 -0600

    Add #create_date #date

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,2 +1,17 @@
 class Shift
-end 
+  attr_reader :key,
+              :date
+
+  def initalize
+    @key = key
+    @date = date
+  end
+
+  def create_key
+    @key = rand(99999).to_s.rjust(5, '0')
+  end
+
+  def create_date
+    @date = Date::today.strftime('%d%m%y')
+  end
+end
