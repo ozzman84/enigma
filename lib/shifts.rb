148cf59444eb13767a464f9eab0ddb0e02a8d734
Ozzie <mikeosmonson@gmail.com>
2021-06-14 16:58:12 -0600

    add #create_shift_hash

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,11 +1,14 @@
 class Shift
   attr_reader :key,
-              :date
+              :date,
+              :key_hash,
+              :offsets_hash
 
   def initalize
-    @key      = key
-    @date     = date
-    @key_hash = key_hash
+    @key          = key
+    @date         = date
+    @key_hash     = Hash.new(0)
+    @offsets_hash = Hash.new(0)
   end
 
   def create_key
@@ -16,24 +19,28 @@ class Shift
     @date = Date::today.strftime('%d%m%y')
   end
 
-  def create_key_hash(key)
-    key_arr   = key.split('').map(&:to_i)
+  def create_key_hash
+    key_arr   = @key.split('').map(&:to_i)
     @key_hash = {
-      A: key_arr.values_at(0, 1).join.to_i,
-      B: key_arr.values_at(1, 2).join.to_i,
-      C: key_arr.values_at(2, 3).join.to_i,
-      D: key_arr.values_at(3, 4).join.to_i
+      :A => key_arr.values_at(0, 1).join.to_i,
+      :B => key_arr.values_at(1, 2).join.to_i,
+      :C => key_arr.values_at(2, 3).join.to_i,
+      :D => key_arr.values_at(3, 4).join.to_i
     }
   end
 
-  def create_offsets_hash(date)
-    date_arr  = date.to_i * date.to_i
-    date_arr  = date_arr.to_s.split('')
+  def create_offsets_hash
+    date_arr      = @date.to_i * @date.to_i
+    date_arr      = date_arr.to_s.split('')
     @offsets_hash = {
-      D: date_arr.pop.to_i,
-      C: date_arr.pop.to_i,
-      B: date_arr.pop.to_i,
-      A: date_arr.pop.to_i
+      :D => date_arr.pop.to_i,
+      :C => date_arr.pop.to_i,
+      :B => date_arr.pop.to_i,
+      :A => date_arr.pop.to_i
     }
+  end
+
+  def create_shift_hash
+    @key_hash.merge(@offsets_hash)
   end
 end
