ad67172848f8ba92b6ea58e1ed14887d7cbe6247
Ozzie <mikeosmonson@gmail.com>
2021-06-14 14:40:55 -0600

    Add #create_hash_keys

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -5,6 +5,7 @@ class Shift
   def initalize
     @key = key
     @date = date
+    @key_hash = Hash.new(0)
   end
 
   def create_key
@@ -13,5 +14,15 @@ class Shift
 
   def create_date
     @date = Date::today.strftime('%d%m%y')
+  end
+
+  def create_key_hash(key)
+    key = key.split('').map(&:to_i)
+    @key_hash = {
+      A: key.values_at(0, 1).join.to_i,
+      B: key.values_at(1, 2).join.to_i,
+      C: key.values_at(2, 3).join.to_i,
+      D: key.values_at(3, 4).join.to_i
+    }
   end
 end
