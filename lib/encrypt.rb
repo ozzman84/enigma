0b6964cf224bc3869d98d86ae49c1cc48b00f06f
Ozzie <mikeosmonson@gmail.com>
2021-06-14 23:26:55 -0600

    Add sum index

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,6 +3,7 @@ class Encrypt
 
   def initialize(message)
     @message = message
+    @shift_index = [10, 20, 0, 10]
   end
 
   def char
@@ -16,6 +17,16 @@ class Encrypt
   def index
     message_arr.map do |letter|
       char.rindex(letter)
+    end
+  end
+
+  def new_index
+    arr = index.zip(@shift_index.cycle)
+  end
+
+  def sum_index
+    new_index.map do |x|
+      x.reduce(:+)
     end
   end
 end
