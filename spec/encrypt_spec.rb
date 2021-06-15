d2cd23725a62eb11bf177e9f49e2b716fc5548dc
Ozzie <mikeosmonson@gmail.com>
2021-06-14 20:09:04 -0600

    Add #attribute tests

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -7,8 +7,21 @@ SimpleCov.start
 
 RSpec.describe Encrypt do
   it 'exists' do
-    encrypt = Encrypt.new
+    encrypt = Encrypt.new('hello')
 
     expect(encrypt).to be_a(Encrypt)
+  end
+
+  it 'has readable attributes' do
+    encrypt = Encrypt.new('hello')
+
+    expect(encrypt.message).to eq('hello')
+    expect(encrypt.char).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it 'can return index values' do
+    encrypt = Encrypt.new('hello')
+
+    expect(encrypt.message_values).to eq(['h', 'e', 'l', 'l', 'o'])
   end
 end
